0eb1538f2d0508b7d839182ab978c011459e701e
Marlon Miles MG <magonjomarlon@gmail.com>
2019-08-27 13:19:28 +0200

    Controller and Views

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -17,7 +17,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddXmlDataContractSerializerFormatters();  // To return data in xml format add AddXmlDataContractSerializerFormatters()
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
