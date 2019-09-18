6e45df42f11f28be31dafd54abab8039be6acca5
Marlon Miles MG <magonjomarlon@gmail.com>
2019-09-18 13:01:02 +0200

    Global Exception Handling and Logging in ASP.Net Core MVC

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,6 +22,19 @@ namespace EmployeeManagement.Controllers
                     break;
             }
             return View("NotFound");
+        }
+
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            ViewBag.ExceptionPath = exceptionDetails.Path;
+            ViewBag.ExceptionMeassage = exceptionDetails.Error.Message;
+            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+
+            return View("Error");
         }
     }
 }
